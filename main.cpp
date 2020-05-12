bedc53442df4ae86dceb89ca1b42871bb99fefc7
Gabriel Antonio <gmav1010@gmail.com>
2020-05-12 08:16:43 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main() {
+	double rendaSala, rendaServ, rendaCapital, gMedico, gEduca;
+	cout << "Renda anual com salario: ";
+	cin >> rendaSala;
+	cout << "Renda anual com prestacao de servicos: ";
+	cin >> rendaServ;
+	cout << "Renda anual com ganho de capital: ";
+	cin >> rendaCapital;
+	cout << "Gastos medicos: ";
+	cin >> gMedico;
+	cout << "Gastos educacionais: ";
+	cin >> gEduca;
+	
 	return 0;
 }
